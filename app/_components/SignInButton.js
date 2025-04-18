ad729e1bfa36c9cdb58889f5e50329421643ec4c
Ford-CcpMmk <Fordccp.mmk@gmail.com>
2025-04-18 23:49:17 +0700

    sign in & sign out & creating a new guest on first sign in

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,14 +1,14 @@
+import { signInAction } from "../_lib/actions";
+
 function SignInButton() {
+  // We create a server aciton to add interactivity also to server components, and usually to forms. And again, once the button will be clicked, then the form will automatically get submitted. And what will happen once that submission is happening is what we pass into the action prop here. So here we can now pass in a special function which is called a server action.
   return (
-    <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
-      <img
-        src='https://authjs.dev/img/providers/google.svg'
-        alt='Google logo'
-        height='24'
-        width='24'
-      />
-      <span>Continue with Google</span>
-    </button>
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img src="https://authjs.dev/img/providers/google.svg" alt="Google logo" height="24" width="24" />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -13,10 +14,41 @@ const authConfig = {
     authorized({ auth, request }) {
       return !!auth?.user;
     },
+    // This function is called before they're actually signed in to the application.
+    async signIn({ user, account, profile }) {
+      // What we're gonna do here is to create a new guest when the user signs in for the first time. So when there isn't a guest with that email here yet on the guests supabase table, and while if there is one, then we don't do anything here.
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+        }
+
+        //  In order to move on in the sign in process, we need to return true here.
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    // it runs after the sign in callback, and also each time that the session is checked out So, for example, when we call that auth function.
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
+  },
+  // Tell our auth.js that instead of going to default login page, go to our custom page.
+  pages: {
+    signIn: "/login",
   },
 };
 
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
