5a33b31e5bdfe4101297a51b89c153b3f33dc0b1
Ford-CcpMmk <Fordccp.mmk@gmail.com>
2025-04-19 10:47:57 +0700

    Update the profile using a server action (not done yet)

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,13 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
-export default function UpdateProfileForm({ children }) {
+export default function UpdateProfileForm({ guest, children }) {
   const [count, setCounter] = useState(0);
 
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
     <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
@@ -15,6 +13,7 @@ export default function UpdateProfileForm({ children }) {
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -23,6 +22,7 @@ export default function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -30,11 +30,7 @@ export default function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img
-            src={countryFlag}
-            alt="Country flag"
-            className="h-5 rounded-sm"
-          />
+          <img src={countryFlag} alt="Country flag" className="h-5 rounded-sm" />
         </div>
 
         {children}
@@ -43,6 +39,7 @@ export default function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,33 +1,31 @@
 import SelectCountry from "@/app/_components/SelectCountry";
 import UpdateProfileForm from "@/app/_components/UpdateProfileForm";
+import { auth } from "@/app/_lib/auth";
+import { getGuest } from "@/app/_lib/data-service";
 
 export const metadata = {
   title: "Update profile",
 };
 
-export default function Page() {
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+export default async function Page() {
+  const session = await auth();
+  const guest = await getGuest(session.user.email);
 
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-4">
-        Update your guest profile
-      </h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-4">Update your guest profile</h2>
 
       <p className="text-lg mb-8 text-primary-200">
-        Providing the following information will make your check-in process
-        faster and smoother. See you soon!
+        Providing the following information will make your check-in process faster and smoother. See you soon!
       </p>
 
       {/* It works because we're now importing a server component inside another server component. And so SelectCountry component will then run here and is then being passed into the client component. And that's no problem at all.Again, because all the work in here has already happened. So the data fetching has happened on the server. */}
-      <UpdateProfileForm>
+      <UpdateProfileForm guest={guest}>
         <SelectCountry
           name="nationality"
           id="nationality"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          defaultCountry={nationality}
+          defaultCountry={guest.nationality}
         />
       </UpdateProfileForm>
     </div>
